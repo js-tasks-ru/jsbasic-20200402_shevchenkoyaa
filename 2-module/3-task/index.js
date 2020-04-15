08e45b9d3a0c3abe3cd729281e69aff813d8fef0
Shevchenko Yaroslav <shevchenkoyaa@gmail.com>
2020-04-15 10:59:20 +0300

    калькулятор

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,19 @@
 let calculator = {
-  // ваш код
+ a: 3,
+ b: 5, 
 };
+  
+   calculator.read = function (read) {
+     return (this.a, this.b);
+   }; 
+   calculator.sum = function (sum) {
+     return (this.a + this.b);
+   };
+   calculator.mul = function (mul) {
+     return (this.a * this.b)
+   } 
+
+ 
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
