bbe9edf9947d5808cc8a2ab4298e7db8398b9024
Shevchenko Yaroslav <shevchenkoyaa@gmail.com>
2020-04-23 20:34:10 +0300

    список друзей

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,5 +4,15 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
-  // ваш код...
-}
+  let ul = document.createElement('ul');
+  for (i = 0; i < friends.length; i++) {
+  
+    ul.innerHTML = `<li> ${friends[i].firstName} ${friends[i].lastName} </li>`
+    
+  return ul.innerHTML
+  
+  }
+  
+  
+  }
+  
