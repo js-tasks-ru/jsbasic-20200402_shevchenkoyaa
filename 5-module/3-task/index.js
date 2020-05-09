748764e2dcb6decf900029ac5a5ebe13540ff2d4
Shevchenko Yaroslav <shevchenkoyaa@gmail.com>
2020-05-09 14:42:38 +0300

    карусель 5.3 done

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -7,7 +7,12 @@ let offsetWidth = carousel.offsetWidth
 
 
 rightClickButton.addEventListener('click', counterPlus);
+rightClickButton.addEventListener('click', sliding);
+rightClickButton.addEventListener('click', arrowHide);
 leftClickButton.addEventListener('click', counterMinus);
+leftClickButton.addEventListener('click', sliding);
+leftClickButton.addEventListener('click', arrowHide);
+
 
 function counterPlus() {
    if (counter < 3) {
@@ -26,11 +31,11 @@ function sliding() {
    if (counter === 0) {
       carousel.style.transform = 'translateX(0px)'
    } else if (counter === 1) {
-      carousel.style.transform = `translateX(988px)`
+      carousel.style.transform = `translateX(-988px)`
    } else if (counter === 2) {
-      carousel.style.transform = `translateX(${988 * 2}px)` 
+      carousel.style.transform = `translateX(${-988 * 2}px)` 
    } else if (counter === 3) {
-      carousel.style.transform = `translateX(${988 * 3}px)` 
+      carousel.style.transform = `translateX(${-988 * 3}px)` 
    }
 } 
 
@@ -39,6 +44,10 @@ function arrowHide() {
       leftClickButton.style.display = 'none'
    } else if (counter === 3) {
       rightClickButton.style.display = 'none'
+   } else if (counter === 1) {
+      leftClickButton.style.display = ''
+   } else if (counter === 2) {
+      rightClickButton.style.display = ''
    }
    
 }
