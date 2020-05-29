9ff12a1133dc241958f259b30eaf7be50e9abf58
Shevchenko Yaroslav <shevchenkoyaa@gmail.com>
2020-05-29 13:15:54 +0300

    6.2

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -4,17 +4,35 @@ export default class ProductCard {
   constructor(product) {
     this.product = product;
     this.elem = document.createElement('div');
+    this.render(product);
+    this.elem.addEventListener('click', event => this.Onclick(event))
+  }
+
+
+    render(product) {
     this.elem.innerHTML = `<div class="card">
     <div class="card__top">
-        <img src="/assets/images/products/...значение product.image..." class="card__image" alt="product">
-        <span class="card__price">€<!--значение product.price--></span>
+        <img src="/assets/images/products/${product.name} class="card__image" alt="product">
+        <span class="card__price">€${product.price.toFixed(2)}</span>
     </div>
     <div class="card__body">
-        <div class="card__title"><!--значение product.name--></div>
+        <div class="card__title"><${product.name}></div>
         <button type="button" class="card__button">
             <img src="/assets/images/icons/plus-icon.svg" alt="icon">
         </button>
     </div>
-</div>`
-  }
+</div>`;
+    }
+
+    /*Onclick(event) {
+      if (event.target.closest('.card__button')) {
+        let customEvent = new CustomEvent("product-add", { 
+        detail: this.product.id, 
+        bubbles: true)
+        }
+        elem.dispatchEvent(customEvent);
+     }
+     
+    }
+*/
 }
